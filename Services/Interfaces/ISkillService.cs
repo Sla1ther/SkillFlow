150cebb889a96946a25f8c7bbc965f6c0799e503
Sla1ther <cem31071982@gmail.com>
2026-05-21 20:32:28 +0300

    addded ISkillService interface

---
+++ Services/Interfaces/ISkillService.cs
@@ -0,0 +1,17 @@
+﻿using SkillFlow.Models;
+namespace SkillFlow.Services.Interfaces
+{
+    /// <summary>
+    /// ISkillService defines the contract for skill-related business logic operations, including methods for 
+    /// retrieving, creating, updating, and deleting skill entities. 
+    /// This interface abstracts the underlying data access implementation,
+    /// </summary>
+    public interface ISkillService
+    {
+        public IEnumerable<SkillModel> GetAllSkills();
+        public SkillModel GetSkillById(int id);
+        public void CreateSkill(SkillModel skill);
+        public void UpdateSkill(SkillModel skill);
+        public void DeleteSkill(int id);
+    }
+}
